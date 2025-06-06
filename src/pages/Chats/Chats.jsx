f76dc0d32ea2554afac1d34c4098f37e5677829f
Abanoub Nagy <96650563+AbanoubNagyHalem@users.noreply.github.com>
2025-06-06 14:39:51 +0300

    LeftSidebar

--- src/pages/Chats/Chats.jsx
+++ src/pages/Chats/Chats.jsx
@@ -1,7 +1,18 @@
+import ChatBox from "../../components/ChatBox/ChatBox";
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+import RightSidebar from "../../components/RightSidebar/RightSidebar";
 import "./Chats.css";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  return (
+    <div className="chat">
+      <div className="chat-container">
+        <LeftSidebar />
+        <ChatBox />
+        <RightSidebar />
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
